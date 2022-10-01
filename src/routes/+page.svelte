a8e4f74265661026587663646e23bf3a52215506
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-09-30 21:28:40 -0400

    Template
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+
+	let svg;
+	let width = 960;
+	let height = 500;
+
+	onMount(() => {
+		svg = d3.select('#chart').append('svg').attr('width', width).attr('height', height);
+	});
+</script>
+
+<svg id="chart" />
